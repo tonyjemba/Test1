9ceece995e26a41089e92968a5dd6f74108050af
Tony Jemba <jjembatony@gmail.com>
2022-08-07 06:45:29 +0300

    solution to problem 1 with quadratic time complexity

---
+++ solution.js
@@ -0,0 +1,61 @@
+// Create function to convert given string to the output below
+// Output Example
+/* const output = {
+  and: [
+    1069,
+    { or: [1070, 1071, 1072] },
+    1244,
+    { or: [1245, 1339] },
+  ]
+}; */
+
+// Input
+const optionRule = '{1069} AND ({1070} OR {1071} OR {1072}) AND {1244} AND ({1245} OR {1339})';
+
+let result = function(optionRule){
+//check input
+if(typeof optionRule !== 'string' || optionRule.length === 0 ){
+return "invalid input"
+}
+
+//split the string using "AND"
+const and_splitted = optionRule.split("AND")
+
+//loop over the splittedString to find stings with "OR" and split them too
+const or_splitted =  and_splitted.map(str => {
+//if the string has OR split else return the str
+if(str.includes('OR')){
+const splitted = str.split("OR")
+return {splitted}
+}
+return str
+})
+
+//combined results
+const result = Object.assign({...and_splitted},{...or_splitted})
+
+//flattening the object
+const flat_result = Object.values(result).flat()
+
+// removing unwanted characters from strings
+
+const cleaned_result = flat_result.map(val=>{''
+//if its a string remove the \{} and manke it a number 
+if(typeof val === 'string'){
+const clean =  Number(val.replace(/[{}]/g, ''))
+return clean
+}
+
+//if its an object make it flat and remove the unwanted digits
+const clean2 = Object.values(val).flat().toString().replace(/[{()}\s+]/g, '').split(",")
+return {'or':[...clean2.map(i=>Number(i))]}
+
+})
+
+//spread the result into an object
+const inObj = {and:[...cleaned_result]}
+
+return inObj
+}
+
+console.log('result:', result(optionRule));
