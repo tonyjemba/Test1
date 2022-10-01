788c13c532a9a641f0349a0adde0cc1f65abbb9e
Tony Jemba <jjembatony@gmail.com>
2022-10-01 12:34:37 +0300

    using the standard preset

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// it shows an error because it is against the standard preset to use ; in js files
+createApp(App).mount('#app');
--- src/store/counter.js
+++ src/store/counter.js
@@ -4,8 +4,7 @@ export const useCounterStore = defineStore('counter', {
   state: () => {
     return { count: 0 }
   },
-  // could also be defined as
-  // state: () => ({ count: 0 })
+
   actions: {
     increment () {
       if (this.count === 0) {
