461f54f8ce9c593aee2165a8c033ff5a4290242f
Tony Jemba <jjembatony@gmail.com>
2022-10-06 13:23:33 +0300

    formatting

--- src/components/TodoListItems.vue
+++ src/components/TodoListItems.vue
@@ -1,25 +1,19 @@
 <script setup>
-
 defineProps({
-  listItems: {type:Array,required:true}
-})
-
-
+    listItems: { type: Array, required: true },
+});
 </script>
 
-<template v-for>
-  <h1>Todo List</h1>
+<template>
+    <h1>Todo List</h1>
 
-  <ul
-    v-for="listItem in listItems"
-    :key="listItem.id"
-  >
-    <li>{{ listItem.value }}</li>
-  </ul>
+    <ul v-for="listItem in listItems" :key="listItem.id">
+        <li>{{ listItem.value }}</li>
+    </ul>
 </template>
 
 <style scoped>
 .read-the-docs {
-  color: #888;
+    color: #888;
 }
 </style>
